1da64a84baf730cf718b25cec793f0986dd90451
Christian Schou <christian.schou@hotmail.com>
2024-02-04 13:35:39 +0100

    Refactored to support async and new CreateRequest method

--- GhostLyzer.Module.GhostApi/Resources/Shared/Pages.cs
+++ GhostLyzer.Module.GhostApi/Resources/Shared/Pages.cs
@@ -13,9 +13,21 @@ namespace GhostLyzer.Module.GhostApi.Services
         /// <returns>A response containing all pages.</returns>
         public PageResponse GetPages(PostQueryParams queryParams = null)
         {
-            var request = CreateRequest("pages", Method.Get);
+            var request = CreateRequest(Method.Get, "pages");
             ApplyPageQueryParams(request, queryParams);
             return Execute<PageResponse>(request);
+        }
+
+        /// <summary>
+        /// Gets all pages async.
+        /// </summary>
+        /// <param name="queryParams">Optional query parameters.</param>
+        /// <returns>A response containing all pages.</returns>
+        public async Task<PageResponse> GetPagesAsync(PostQueryParams queryParams = null)
+        {
+            var request = CreateRequest(Method.Get, "pages");
+            ApplyPageQueryParams(request, queryParams);
+            return await ExecuteAsync<PageResponse>(request);
         }
 
         /// <summary>
@@ -26,9 +38,23 @@ namespace GhostLyzer.Module.GhostApi.Services
         /// <returns>The page with the given ID.</returns>
         public Post GetPageById(string id, PostQueryParams queryParams = null)
         {
-            var request = CreateRequest($"pages", Method.Get, id);
+            var request = CreateRequest(Method.Get, "pages", id);
             ApplyPageQueryParams(request, queryParams);
             return Execute<PageResponse>(request)?.Pages?.Single();
+        }
+
+        /// <summary>
+        /// Gets a page by its ID async.
+        /// </summary>
+        /// <param name="id">The ID of the page.</param>
+        /// <param name="queryParams">Optional query parameters.</param>
+        /// <returns>The page with the given ID.</returns>
+        public async Task<Post> GetPageByIdAsync(string id, PostQueryParams queryParams = null)
+        {
+            var request = CreateRequest(Method.Get, "pages", id);
+            ApplyPageQueryParams(request, queryParams);
+            var response = await ExecuteAsync<PageResponse>(request);
+            return response?.Pages?.Single();
         }
 
         /// <summary>
@@ -39,9 +65,23 @@ namespace GhostLyzer.Module.GhostApi.Services
         /// <returns>The page with the given slug.</returns>
         public Post GetPageBySlug(string slug, PostQueryParams queryParams = null)
         {
-            var request = CreateRequest($"pages/slug", Method.Get, slug);
+            var request = CreateRequest(Method.Get, "pages/slug", slug);
             ApplyPageQueryParams(request, queryParams);
             return Execute<PageResponse>(request)?.Pages?.Single();
+        }
+
+        /// <summary>
+        /// Gets a page by its slug sync.
+        /// </summary>
+        /// <param name="slug">The slug of the page.</param>
+        /// <param name="queryParams">Optional query parameters.</param>
+        /// <returns>The page with the given slug.</returns>
+        public async Task<Post> GetPageBySlugAsync(string slug, PostQueryParams queryParams = null)
+        {
+            var request = CreateRequest(Method.Get, "pages/slug", slug);
+            ApplyPageQueryParams(request, queryParams);
+            var response = await ExecuteAsync<PageResponse>(request);
+            return response.Pages?.Single();
         }
 
         /// <summary>
