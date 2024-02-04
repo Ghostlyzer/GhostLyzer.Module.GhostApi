3348cec1b83ff54ce88a02436b7eae8eef1b61f0
Christian Schou <christian.schou@hotmail.com>
2024-02-04 13:36:44 +0100

    Add Site Admin API

---
+++ GhostLyzer.Module.GhostApi/Resources/Admin/Site.cs
@@ -0,0 +1,29 @@
+﻿using GhostLyzer.Module.GhostApi.Models;
+using RestSharp;
+
+namespace GhostLyzer.Module.GhostApi.Services
+{
+    public partial class GhostAdminAPI
+    {
+        /// <summary>
+        /// Retrieve basic information about a site.
+        /// </summary>
+        /// <returns>Basic information about a site. (i.e. title, version, URL, etc...)</returns>
+        public Site GetSite()
+        {
+            var request = CreateRequest(Method.Get, "site");
+            return Execute<SiteResponse>(request).Site;
+        }
+
+        /// <summary>
+        /// Retrieve basic information about a site async.
+        /// </summary>
+        /// <returns>Basic information about a site. (i.e. title, version, URL, etc...)</returns>
+        public async Task<Site> GetSiteAsync()
+        {
+            var request = CreateRequest(Method.Get, "site");
+            var response = await ExecuteAsync<SiteResponse>(request);
+            return response.Site;
+        }
+    }
+}
