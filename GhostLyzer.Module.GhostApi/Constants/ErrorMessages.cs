cda2f939d3f05dd27be20a8ff53d3621b0843d87
Christian Schou <christian.schou@hotmail.com>
2024-02-04 09:56:53 +0100

    Add error msg for updated entities
    This error message is only used when the post has been updated in the meantime from retrieving it to the service would like to update it again.

---
+++ GhostLyzer.Module.GhostApi/Constants/ErrorMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace GhostLyzer.Module.GhostApi.Constants
+{
+    public class ErrorMessages
+    {
+        public const string OriginalPostHasBeenUpdated =
+            "The original post/page has been updated (the timestamp is more recent). If you wish to force an update through, change the UpdatedAt timestamp and try again.";
+    }
+}
