96735c2aa673601e1448cd12247beac0d7317b02
Christian Schou <christian.schou@hotmail.com>
2024-02-04 19:34:54 +0100

    Add async method and regions

--- GhostLyzer.Module.GhostApi/Resources/Admin/Images.cs
+++ GhostLyzer.Module.GhostApi/Resources/Admin/Images.cs
@@ -6,23 +6,42 @@ namespace GhostLyzer.Module.GhostApi.Services
 {
     public partial class GhostAdminAPI
     {
-        private static readonly Dictionary<ImageType, string> MimeTypes = new Dictionary<ImageType, string>
-        {
-            { ImageType.GIF, "image/gif" },
-            { ImageType.ICO, "image/x-icon" },
-            { ImageType.JPEG, "image/jpeg" },
-            { ImageType.PNG, "image/png" },
-            { ImageType.SVG, "image/svg+xml" },
-            { ImageType.Unknown, "application/octet-stream" },
-        };
-
         /// <summary>
-        /// Upload an image to the site
+        /// Uploads an image to the site.
         /// </summary>
-        /// <returns>Returns location from which the image can be fetched, and reference string if any.</returns>
+        /// <param name="image">The image to upload.</param>
+        /// <returns>The uploaded image.</returns>
         public Image UploadImage(ImageRequest image)
         {
-            var request = new RestRequest("images/upload/", Method.Post);
+            var request = PrepareUploadImageRequest(image);
+            return Execute<ImageResponse>(request).Images[0];
+        }
+
+        /// <summary>
+        /// Uploads an image to the site asynchronously.
+        /// </summary>
+        /// <param name="image">The image to upload.</param>
+        /// <returns>The uploaded image.</returns>
+        public async Task<Image> UploadImageAsync(ImageRequest image)
+        {
+            var request = PrepareUploadImageRequest(image);
+            var response = await ExecuteAsync<ImageResponse>(request);
+            return response.Images[0];
+        }
+
+        private static string GetMimeType(ImageType imageType)
+        {
+            return MimeTypes[imageType];
+        }
+
+        /// <summary>
+        /// Prepares a RestRequest for uploading an image.
+        /// </summary>
+        /// <param name="image">The image to upload.</param>
+        /// <returns>A RestRequest that can be used to upload an image.</returns>
+        private RestRequest PrepareUploadImageRequest(ImageRequest image)
+        {
+            var request = CreateRequest(Method.Post, "images/upload");
 
             if (image.FilePath != null)
                 request.AddFile(nameof(image.File), image.FilePath, GetMimeType(image.ImageType));
@@ -32,12 +51,17 @@ namespace GhostLyzer.Module.GhostApi.Services
             request.AddParameter("purpose", image.Purpose.ToString().ToLower());
             request.AddParameter("ref", image.Reference);
 
-            return Execute<ImageResponse>(request).Images[0];
+            return request;
         }
 
-        private static string GetMimeType(ImageType imageType)
+        private static readonly Dictionary<ImageType, string> MimeTypes = new Dictionary<ImageType, string>
         {
-            return MimeTypes[imageType];
-        }
+            { ImageType.GIF, "image/gif" },
+            { ImageType.ICO, "image/x-icon" },
+            { ImageType.JPEG, "image/jpeg" },
+            { ImageType.PNG, "image/png" },
+            { ImageType.SVG, "image/svg+xml" },
+            { ImageType.Unknown, "application/octet-stream" },
+        };
     }
 }
