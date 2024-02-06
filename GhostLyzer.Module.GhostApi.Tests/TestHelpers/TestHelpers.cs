b236caf31b466c8458c408b6b4807d76217baa59
Christian Schou <christian.schou@hotmail.com>
2024-02-06 20:56:34 +0100

    Add test helpers

---
+++ GhostLyzer.Module.GhostApi.Tests/TestHelpers/TestHelpers.cs
@@ -0,0 +1,10 @@
+﻿namespace GhostLyzer.Module.GhostApi.Tests.TestHelpers
+{
+    public static class TestHelpers
+    {
+        public static DateTime RemoveTicks(this DateTime dateTime)
+        {
+            return dateTime.AddTicks(-(dateTime.Ticks % TimeSpan.TicksPerSecond));
+        }
+    }
+}
